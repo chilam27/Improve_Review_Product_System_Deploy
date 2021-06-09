5a51d177e134f1b4d6844a4f66d03fb446b25559
Dao Chi Lam <66649712+chilam27@users.noreply.github.com>
2021-06-09 14:52:47 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "[server]"  > ~/.streamlit/config.toml 
-echo "headless = true"  >> ~/.streamlit/config.toml
-echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = false"  >> ~/.streamlit/config.toml
+echo "\
+[general]\n\
+email = \"theonlyasian.dc@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
